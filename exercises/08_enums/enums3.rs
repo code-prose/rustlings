eb8d94bfdedfae5c9571a11a042544bc0b4c6b19
Logan Rose <logprose@umich.edu>
2026-01-02 20:35:49 -0500

    feat: finished 08

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,13 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+        match message {
+            Message::Resize {width, height} => self.resize(width, height),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(string) => self.echo(string),
+            Message::ChangeColor(x1, x2, x3) => self.change_color(x1, x2, x3),
+            Message::Quit => self.quit()
+        };
     }
 }
 
